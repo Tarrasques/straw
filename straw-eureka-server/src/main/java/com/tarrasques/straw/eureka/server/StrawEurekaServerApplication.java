9a611d6be669e565b84cb0a53e6c05eb3b3183c0
Tarrasques <1417223554@qq.com>
2021-04-14 15:48:07 +0800

    更新代码

---
+++ straw-eureka-server/src/main/java/com/tarrasques/straw/eureka/server/StrawEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.tarrasques.straw.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrawEurekaServerApplication.class, args);
+    }
+
+}
