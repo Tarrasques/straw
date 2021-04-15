87f78445ea170cd15cba3f31441fa4a03825fe60
Tarrasques <1417223554@qq.com>
2021-04-15 11:00:09 +0800

    更新代码

--- straw-eureka-server/src/main/java/com/tarrasques/straw/eureka/server/StrawEurekaServerApplication.java
+++ straw-eureka-server/src/main/java/com/tarrasques/straw/eureka/server/StrawEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.tarrasques.straw.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class StrawEurekaServerApplication {
 
     public static void main(String[] args) {
